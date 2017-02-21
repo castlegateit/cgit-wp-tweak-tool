2e1a22d44045ba86a6c53607205d6ea5a36d2506
John Hughes <john@castlegateit.co.uk>
2017-02-21 14:42:56 +0000

    Update version number

--- cgit-wp-tweak-tool.php
+++ cgit-wp-tweak-tool.php
@@ -5,7 +5,7 @@
 Plugin Name: Castlegate IT WP Tweak Tool
 Plugin URI: http://github.com/castlegateit/cgit-wp-tweak-tool
 Description: Tweaks various parts of the WordPress dashboard.
-Version: 1.2
+Version: 1.3
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
